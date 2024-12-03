def58b995a166c4f50c5cd6504c6770b33a39b94
MayankPrajapati18 <mayanprajapati2110@gmail.com>
2024-12-03 12:15:55 +0530

    "Updated Dashboard page layout and styling"

--- app/admin/dashboard/page.jsx
+++ app/admin/dashboard/page.jsx
@@ -42,31 +42,33 @@ const Dashboard = () => {
   return (
     <Adminlayout>
       <div className="p-6">
-        <h1 className="text-2xl font-bold mb-8">Dashboard</h1>
+        <h1 className="text-3xl font-bold mb-8">Dashboard</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          {/* Jobs Section */}
           <div
             onClick={() => router.push("/admin/jobs")}
-            className="cursor-pointer flex items-center bg-white border-l-4 border-green-500 shadow-lg h-24 p-4 rounded-lg"
+            className="cursor-pointer flex items-center bg-white border-l-4 border-green-500 shadow-lg h-28 p-6 rounded-lg"
           >
             <div className="flex-grow">
-              <div className="text-xs font-semibold text-green-500 uppercase mb-1">
+              <div className="text-lg font-semibold text-green-500 uppercase mb-2">
                 Jobs
               </div>
-              <div className="text-lg font-bold text-gray-800">
+              <div className="text-2xl font-bold text-gray-800">
                 {jobs?.length ? jobs?.length : 0}
               </div>
             </div>
           </div>
 
+          {/* Job-Applications Section */}
           <div
             onClick={() => router.push("/admin/job-application")}
-            className="cursor-pointer flex items-center bg-white border-l-4 border-yellow-500 shadow-lg h-24 p-4 rounded-lg"
+            className="cursor-pointer flex items-center bg-white border-l-4 border-yellow-500 shadow-lg h-28 p-6 rounded-lg"
           >
             <div className="flex-grow">
-              <div className="text-xs font-semibold text-yellow-500 uppercase mb-1">
+              <div className="text-lg font-semibold text-yellow-500 uppercase mb-2">
                 Job-Applications
               </div>
-              <div className="text-lg font-bold text-gray-800">
+              <div className="text-2xl font-bold text-gray-800">
                 {Jobapplications?.length ? Jobapplications?.length : 0}
               </div>
             </div>
